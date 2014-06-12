563ac4950cf3cf71d2b659fb772ec069b267839a
Lukas Elmer <lukas.elmer@gmail.com>
2014-06-13 01:09:14 +0200

    100 most frequent words

--- src/spark_example/src/main/scala/SimpleApp.scala
+++ src/spark_example/src/main/scala/SimpleApp.scala
@@ -5,16 +5,26 @@ import org.apache.spark.SparkConf
 object SimpleApp {
   def createSparkContext(): SparkContext = {
     val conf = new SparkConf().setAppName("Simple Application")
-    if (System.getenv("USE_LOCAL_MASTER") == "1") conf.setMaster("local")
+
+    // Master is not set => use local master, and local data
+    if (!conf.contains("spark.master")) {
+      conf.setMaster("local[*]")
+      conf.set("data", "data/example.txt")
+    } else {
+      conf.set("data", "/mnt/cw12/cw-data/ClueWeb12_00/0000tw")
+    }
     new SparkContext(conf)
   }
 
   def main(args: Array[String]) {
-    val logFile = "data/example.txt" // Should be some file on your system
     val sc = createSparkContext()
-    val logData = sc.textFile(logFile, 2).cache()
-    val numAs = logData.filter(line => line.contains("a")).count()
-    val numBs = logData.filter(line => line.contains("b")).count()
-    println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+    val logFile = sc.getConf.get("data")
+    val lines = sc.textFile(logFile)
+    val counts = lines.flatMap(line => line.split(" ")).map(word => (word, 1)).reduceByKey(_ + _)
+    val top_words = counts.top(100)(Ordering.by[(String, Int), Int](_._2))
+
+    top_words.foreach(x => {
+      println(x._1 + ":" + x._2)
+    })
   }
 }
