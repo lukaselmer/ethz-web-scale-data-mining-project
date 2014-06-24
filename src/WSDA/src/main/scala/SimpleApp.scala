e8cb5d1889fb383a550676941c36346855986c15
root <root@Hany.Hany>
2014-06-24 18:13:48 +0200

    Include anchors

--- src/WSDA/src/main/scala/SimpleApp.scala
+++ src/WSDA/src/main/scala/SimpleApp.scala
@@ -1,11 +1,17 @@
 import java.net.URL
+import java.util.regex.Pattern
 
+import com.sun.jersey.spi.StringReader
+import de.l3s.boilerpipe.sax.BoilerpipeSAXInput
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.SparkContext._
 import org.xml.sax.InputSource;
 import de.l3s.boilerpipe.extractors;
-
+import java.io.StringReader;
+import de.l3s.boilerpipe.sax.HTMLHighlighter;
 import org.cyberneko.html.HTMLConfiguration
+
+
 object SimpleApp {
 
   def createSparkContext(): SparkContext = {
@@ -16,7 +22,7 @@ object SimpleApp {
     // Master is not set => use local master, and local data
     if (!conf.contains("spark.master")) {
       conf.setMaster("local[*]")
-      conf.set("data", "data/*")
+      conf.set("data", "data/sample.warc")
     } else {
       conf.set("data", "/mnt/cw12/cw-data/ClueWeb12_00/")
     }
@@ -28,6 +34,7 @@ object SimpleApp {
     val sc = createSparkContext()
     val logFile = args(0)
     val min_partitions = args(1).toInt
+    val getAnchors= args(2).toBoolean
     //val min_partitions = 30;
     //val logFile = sc.getConf.get("data")
     val files = sc.wholeTextFiles(logFile, min_partitions)
@@ -37,14 +44,28 @@ object SimpleApp {
                 {
                       try
                       {
-                        extractors.ArticleExtractor.INSTANCE.getText(doc).split(" ")
+                        val textDocument = new BoilerpipeSAXInput(new InputSource(new java.io.StringReader(doc))).getTextDocument()
+                        val documentContent = extractors.ArticleExtractor.INSTANCE.getText(textDocument)
+                        if(getAnchors) {
+                          val hh = HTMLHighlighter.newExtractingInstance()
+                          val highlightedHTML = hh.process(textDocument, doc)
+                          val anchorRegex = "(?s)<\\s*a\\s+.*?href\\s*=\\s*['\"]([^\\s>]*)['\"]";
+                          val anchorPattern = Pattern.compile(anchorRegex, Pattern.CASE_INSENSITIVE);
+
+                          val matcher = anchorPattern.matcher(highlightedHTML);
+                          val anchors = List()
+                          while (matcher.find()) {
+                            anchors.+(matcher.group(1))
+                          }
+                        }
+                        //println(highlightedHTML)
+                        documentContent.split(" ")
                       }
                       catch
                       {
                         case e: Exception=> Array("")
                       }
                 })
-
     val counts = words.map(word => (word, 1)).reduceByKey(_ + _)
 
     val top_words = counts.top(100)(Ordering.by[(String, Int), Int](_._2))
