55cf9e4fff0bb9e5a953ee9cba959f5e8da14e13
Lukas Elmer <lukas.elmer@gmail.com>
2014-07-10 17:58:57 +0200

    delete some config in the code...

--- src/html_to_text_conversion/src/main/scala/HtmlToTextConversionApp.scala
+++ src/html_to_text_conversion/src/main/scala/HtmlToTextConversionApp.scala
@@ -21,8 +21,6 @@ object HtmlToTextConversionApp {
       conf.set("data", "data/example*")
       conf.set("out", "out")
     } else {
-      conf.set("spark.executor.memory", "100g");
-      conf.set("spark.default.parallelism", "200");
       conf.set("data", "file:///mnt/cw12/cw-data/ClueWeb12_00/*")
       conf.set("out", "hdfs://dco-node121:54310/ClueWebConverted/ClueWeb12_00")
       //conf.set("out", "/disk3/user_work/runs/convert_all2")
