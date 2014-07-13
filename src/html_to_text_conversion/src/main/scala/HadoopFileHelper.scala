4cdfe94ffb5af7b58be8dc12f33a243dd71dcb0d
Lukas Elmer <lukas.elmer@gmail.com>
2014-07-13 02:37:28 +0200

    refactoring

--- src/html_to_text_conversion/src/main/scala/HadoopFileHelper.scala
+++ src/html_to_text_conversion/src/main/scala/HadoopFileHelper.scala
@@ -4,9 +4,9 @@ import org.apache.hadoop.fs.{FileSystem, Path}
 import scala.collection.mutable
 
 object HadoopFileHelper {
-  def listHdfsFiles(hdfsPath: Path): List[String] = {
+  def listHdfsFiles(path: Path): List[String] = {
     val fs = FileSystem.get(new Configuration())
-    val i = fs.listFiles(hdfsPath, true)
+    val i = fs.listFiles(path, true)
     val arr = mutable.ListBuffer.empty[String]
     while (i.hasNext)
       arr += i.next.getPath.toString
