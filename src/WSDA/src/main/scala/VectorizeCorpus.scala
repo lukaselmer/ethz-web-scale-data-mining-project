50a628561ca07b223ad136b75f8422d9a5f51156
root <root@Hany.Hany>
2014-07-14 04:41:39 +0200

    Vectorize Corus

--- src/WSDA/src/main/scala/VectorizeCorpus.scala
+++ src/WSDA/src/main/scala/VectorizeCorpus.scala
@@ -1,7 +1,8 @@
 import java.net.URL
 import java.util.regex.Pattern
 import edu.umd.cloud9.math.Gamma;
-import scala.collection.mutable;
+import scala.collection.mutable
+;
 import scala.math;
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.SparkContext._
@@ -36,7 +37,8 @@ object VectorizeCorpus {
   def buildVocabulary(args: Array[String]) {
     val input = args(0)
     val stem = args(1).toBoolean
-    val output = args(2)
+    val vocabOutput = args(2)
+    val output = args(3)
     val sc = createSparkContext();
     //Read vectorized data set
     var vocab = sc.sequenceFile[String, String](input)
@@ -45,8 +47,34 @@ object VectorizeCorpus {
     if(stem)
       vocab = vocab.map(u => PorterStemmer.stem(u));
 
-    vocab = vocab.distinct()
+    vocab = vocab.filter(v => !v.isEmpty()).distinct().repartition(1);
 
-    vocab.saveAsTextFile(output);
+    //Build a hashtable of word_index
+    val dictionary = new mutable.HashMap[String, Int];
+    var index = 0;
+    vocab.collect().foreach(u =>
+      {
+        dictionary.put(u, index);
+        index += 1;
+      });
+    val saved_dictionary = dictionary.toList;
+    sc.parallelize(saved_dictionary, 1).saveAsTextFile(vocabOutput);
+    sc.broadcast(dictionary);
+    //read
+    var files = sc.sequenceFile[String, String](input).flatMap(f => f._2.split(" ").map(w => (f._1, w)));
+    if(stem)
+      files = files
+                .map(k => (k._1, PorterStemmer.stem(k._2)))
+                .filter(u => !u._2.isEmpty());
+
+    val word_counts = files
+                      .map(k => (k._1, dictionary.get(k._2)))
+                      .map(k => ( k, 1)).reduceByKey(_ + _);
+
+    val output_files = word_counts
+      .groupBy(a => a._1._1)
+      .map(f => f._1 + " " + f._2.map(u => u._1._2.get + ":" + u._2).mkString(" "));
+
+    output_files.saveAsTextFile(output);
   }
 }
