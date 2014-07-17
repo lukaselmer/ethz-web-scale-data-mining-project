103880030d1165c3ef2a4c4abb55cd34e51a8b33
root <root@Hany.Hany>
2014-07-17 04:37:45 +0200

    Optimization

--- src/WSDA/src/main/scala/VectorizeCorpus.scala
+++ src/WSDA/src/main/scala/VectorizeCorpus.scala
@@ -16,10 +16,11 @@ import java.io._
 object VectorizeCorpus {
   def createSparkContext(): SparkContext = {
     val conf = new SparkConf().setAppName("Simple Application")
-    conf.set("spark.executor.memory", "10g");
+    //conf.set("spark.executor.memory", "10g");
     conf.set("spark.default.parallelism","200");
     conf.set("spark.akka.frameSize","2000");
     conf.set("spark.akka.timeout","2000");
+
     // Master is not set => use local master, and local data
     if (!conf.contains("spark.master")) {
       conf.setMaster("local[*]")
@@ -39,7 +40,8 @@ object VectorizeCorpus {
   def buildVocabulary(args: Array[String]) {
     val logger = LogManager.getLogger("Vectorize Corpus")
 
-    val HDFS_ROOT = "hdfs://dco-node121.dco.ethz.ch:54310/"
+    //val HDFS_ROOT = "hdfs://dco-node121.dco.ethz.ch:54310/"
+    val HDFS_ROOT = ""
     val input = HDFS_ROOT + args(0)
     val stem = args(1).toBoolean
     val vocabOutput = HDFS_ROOT + args(2)
@@ -66,10 +68,43 @@ object VectorizeCorpus {
     })
 
     val saved_dictionary = dictionary.toList;
-    sc.parallelize(saved_dictionary, 1).saveAsTextFile(vocabOutput);
+    sc.parallelize(saved_dictionary).saveAsTextFile(vocabOutput);
     sc.broadcast(dictionary);
     //read
-    var files = sc.sequenceFile[String, String](input).flatMap(f => f._2.split(" ").map(w => (f._1, w)));
+    //var files = sc.sequenceFile[String, String](input).flatMap(f => f._2.split(" ").map(w => (f._1, w)));
+    val files = sc.sequenceFile[String, String](input);
+    val output_files = files.map(f =>
+    {
+        val file_name = f._1;
+        var emit = file_name;
+        val content = f._2.split(" ");
+
+        val frequency_table = new mutable.HashMap[Int, Int];
+
+        content.foreach(w =>
+        {
+          var cur_word = w;
+          if(stem)
+            cur_word = PorterStemmer.stem(w);
+          if(!cur_word.isEmpty())
+          {
+            val word_index = dictionary.get(cur_word).get;
+            if(frequency_table.containsKey(word_index))
+              frequency_table.update(word_index, frequency_table.get(word_index).get + 1);
+            else
+              frequency_table.put(word_index, 1);
+          }
+        });
+
+      frequency_table.foreach(f => {
+        emit = emit + " " + f._1 + ":" + f._2;
+      });
+      emit;
+    });
+
+
+      //flatMap(f => f._2.split(" ").map(w => (f._1, w)));
+    /*
     if(stem)
       files = files
                 .map(k => (k._1, PorterStemmer.stem(k._2)))
@@ -82,7 +117,7 @@ object VectorizeCorpus {
     val output_files = word_counts
       .groupBy(a => a._1._1)
       .map(f => f._1 + " " + f._2.map(u => u._1._2.get + ":" + u._2).mkString(" "));
-
+    */
     output_files.saveAsTextFile(output);
   }
 }
