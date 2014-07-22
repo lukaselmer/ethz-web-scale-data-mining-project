947551709e1833590dc7d1ddb830bca28d99b2db
Lukas Elmer <lukas.elmer@gmail.com>
2014-07-22 02:46:10 +0200

    formatting

--- src/remove_infrequent_words/src/test/scala/RemoveInfrequentWordsSpec.scala
+++ src/remove_infrequent_words/src/test/scala/RemoveInfrequentWordsSpec.scala
@@ -3,13 +3,10 @@ import org.scalatest._
 
 
 class RemoveInfrequentWordsSpec extends FlatSpec with Matchers {
-
   "The RemoveInfrequentWordsApp" should "correctly remove words not in the keepWords list" in {
     RemoveInfrequentWordsApp.processWords(
       new Text("bli bla blub blubb blubbb bla bli blub"),
       Set("bli", "blubb", "blubbb")
     ) should be(new Text("bli blubb blubbb bli"))
   }
-
-
 }
