7348187b8abafa5f9b0df2552d7ea35f7de5020f
Lukas Elmer <lukas.elmer@gmail.com>
2014-09-05 12:08:38 +0200

    refactoring

--- src/remove_infrequent_words/src/main/scala/RemoveInfrequentWordsApp.scala
+++ src/remove_infrequent_words/src/main/scala/RemoveInfrequentWordsApp.scala
@@ -11,24 +11,19 @@ object RemoveInfrequentWordsApp {
 
   def main(args: Array[String]) {
     val conf = new SparkConf()
-    val configReader = new CommandLineConfigReader(conf, sys.env)
-    val sc = createSparkContext(conf, configReader)
+    val config = new CommandLineConfigReader(conf, sys.env)
+    val sc = createSparkContext(conf, config)
 
-    val inputCombinedDirectory = configReader.inputCombined
-    val inputWordcountDirectory = configReader.inputWordcount
-    val outputDirectory = configReader.output
-    val minWordCount = configReader.minWordCount
-    val maxWordCount = configReader.maxWordCount
-    val maxDictionarySize = configReader.maxDictionarySize
-
-    val keepWords: Set[String] = loadKeepWords(sc, inputWordcountDirectory, minWordCount, maxWordCount, maxDictionarySize)
+    val inputCombinedDirectory = config.inputCombined
+    val outputDirectory = config.output
+    val keepWords: Set[String] = loadKeepWords(sc, config)
 
     val processFileFunction = (inputFile: String) => processFile(inputFile, outputDirectory, keepWords)
     val filesToProcess = HadoopFileHelper.listHdfsFiles(new Path(inputCombinedDirectory)).filter(s => s.endsWith(".combined"))
     sc.parallelize(filesToProcess, filesToProcess.length).foreach(processFileFunction)
   }
 
-  def loadKeepWords(sc: SparkContext, inputWordcountDirectory: String, minWordCount: Int, maxWordCount: Int, maxDictionarySize: Int): Set[String] = {
+  def loadKeepWords(sc: SparkContext, config: CommandLineConfigReader): Set[String] = {
     def extractCountAndWord = (x: String) => {
       // x will have the form of "(234,word)", without spaces
       val countAndWord = x.split(",")
@@ -43,10 +38,10 @@ object RemoveInfrequentWordsApp {
     // Longest non-coined word in a major dictionary
     val maxWordLength = 30
 
-    sc.textFile(inputWordcountDirectory)
+    sc.textFile(config.inputWordcount)
       .map(extractCountAndWord)
       // why doesn't this work in scala :'-( minWordCount <= countAndWord._1 <= maxWordCount
-      .filter(countAndWord => minWordCount <= countAndWord._1 && countAndWord._1 <= maxWordCount)
+      .filter(countAndWord => config.minWordCount <= countAndWord._1 && countAndWord._1 <= config.maxWordCount)
       // filter long words
       .filter(_._2.length <= maxWordLength)
       .collect.toSeq
@@ -54,7 +49,7 @@ object RemoveInfrequentWordsApp {
       .sorted(Ordering.by[(Int, String), Int](-_._1))
       // only use the words, ignore the counts
       .map(_._2)
-      .take(maxDictionarySize)
+      .take(config.maxDictionarySize)
       .toSet
   }
 
@@ -104,6 +99,4 @@ object RemoveInfrequentWordsApp {
 
     new SparkContext(conf)
   }
-
-
 }
